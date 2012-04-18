c0604eed7daa5e7c8d0d09b539c48318e3b9c218
Matt Wright <matt@localprojects.net>
2012-04-18 19:04:11 -0400

    polish

--- app/__init__.py
+++ app/__init__.py
@@ -23,7 +23,7 @@ class SocialLoginError(Exception):
 def create_app():
     app = Flask(__name__)
     app.config.from_yaml(app.root_path)
-    
+    app.config.from_heroku()
     app.wsgi_app = MethodRewriteMiddleware(app.wsgi_app)
     
     init_assets(app)
