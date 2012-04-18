3478bf108ce6992239c638e6e662a6e53204ae46
Matt Wright <matt@localprojects.net>
2012-04-18 18:52:47 -0400

    Update wsgi.py for port

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,5 @@ from app import create_app
 application = create_app()
 
 if __name__ == '__main__':
-    application.run()
+    port = int(os.environ.get('PORT', 5000))
+    application.run(host='0.0.0.0', port=port)
