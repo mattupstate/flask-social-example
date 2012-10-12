fd2504a817fd39935ded05234bc16cc162047e4d
Matt Wright <matt@nobien.net>
2012-10-12 13:20:23 -0400

    Fix config

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,8 @@ app.config.from_yaml(app.root_path)
 app.config.from_heroku()
 app.wsgi_app = MethodRewriteMiddleware(app.wsgi_app)
 
+app.logger.debug(app.config)
+
 db = SQLAlchemy(app)
 webassets = Environment(app)
 
--- app/helpers.py
+++ app/helpers.py
@@ -1,15 +1,11 @@
 import os
-import sys
-import urlparse
 import yaml
-from functools import wraps
 
-from flask import (Flask as BaseFlask, Config as BaseConfig, 
-                   render_template, flash)
-from werkzeug import BaseResponse
+from flask import Flask as BaseFlask, Config as BaseConfig
+
 
 class Config(BaseConfig):
-    
+
     def from_heroku(self):
         # Register database schemes in URLs.
         for key in ['DATABSE_URL', 'SHARED_DATABASE_URL']:
@@ -27,41 +23,40 @@ class Config(BaseConfig):
                 if ev in os.environ:
                     social_key = 'SOCIAL_' + key_prefix
                     oauth_key = 'consumer_' + key_suffix
-                    self[social_key]['oauth'][oauth_key] = os.environ[ev]
-    
+                    self[social_key][oauth_key] = os.environ[ev]
+
     def from_yaml(self, root_path):
         env = os.environ.get('FLASK_ENV', 'development').upper()
         self['ENVIRONMENT'] = env.lower()
-        
+
         for fn in ('app', 'credentials'):
             config_file = os.path.join(root_path, 'config', '%s.yml' % fn)
-            
+
             try:
                 with open(config_file) as f:
                     c = yaml.load(f)
-                
+
                 c = c.get(env, c)
-                
+
                 for key in c.iterkeys():
                     if key.isupper():
                         self[key] = c[key]
             except:
                 pass
-                    
-                
+
+
 class Flask(BaseFlask):
     """Extended version of `Flask` that implements custom config class
     and adds `register_middleware` method"""
-    
+
     def make_config(self, instance_relative=False):
         root_path = self.root_path
         if instance_relative:
             root_path = self.instance_path
         return Config(root_path, self.default_config)
-    
+
     def register_middleware(self, middleware_class):
         """Register a WSGI middleware on the application
         :param middleware_class: A WSGI middleware implementation
         """
         self.wsgi_app = middleware_class(self.wsgi_app)
-        
