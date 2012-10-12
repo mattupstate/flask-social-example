75cc5d603786335565e19c34135ac0c2e9ec4c39
Matt Wright <matt@nobien.net>
2012-10-12 13:51:02 -0400

    adjust helpers

--- app/helpers.py
+++ app/helpers.py
@@ -8,7 +8,7 @@ class Config(BaseConfig):
 
     def from_heroku(self):
         # Register database schemes in URLs.
-        for key in ['DATABSE_URL', 'SHARED_DATABASE_URL']:
+        for key in ['DATABSE_URL']:
             if key in os.environ:
                 self['SQLALCHEMY_DATABASE_URI'] = os.environ[key]
                 break
