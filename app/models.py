76ea014fa4873ed970a98754e5b9df064270f615
Matt Wright <matt@nobien.net>
2012-10-12 13:00:01 -0400

    Fix models

--- app/models.py
+++ app/models.py
@@ -5,8 +5,8 @@ from . import db
 
 
 roles_users = db.Table('roles_users',
-    db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-    db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+    db.Column('user_id', db.Integer(), db.ForeignKey('users.id')),
+    db.Column('role_id', db.Integer(), db.ForeignKey('roles.id')))
 
 
 class Role(db.Model, RoleMixin):
@@ -42,7 +42,7 @@ class Connection(db.Model):
     __tablename__ = "connections"
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     provider_id = db.Column(db.String(255))
     provider_user_id = db.Column(db.String(255))
     access_token = db.Column(db.String(255))
