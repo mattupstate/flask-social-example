59a2814d7574ebef06f9b3cd83a2da0ea2b85fb7
Matt Wright <matt@nobien.net>
2012-12-12 11:42:35 -0500

    debug statement

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,8 @@ app.config.from_yaml(app.root_path)
 app.config.from_heroku()
 app.wsgi_app = MethodRewriteMiddleware(app.wsgi_app)
 
+app.logger.debug('MODULE=' + app.config['SOCIAL_GITHUB']['module'])
+
 db = SQLAlchemy(app)
 webassets = Environment(app)
 
