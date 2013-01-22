5299f26360f51cbc7dd6486bc3a2be8165d081d8
Erik Taubeneck <erik.taubeneck@gmail.com>
2013-01-22 17:59:51 -0500

    added debug print statement

--- app/views.py
+++ app/views.py
@@ -78,7 +78,8 @@ def profile():
 def social_post(provider_id):
     provider = get_provider_or_404(provider_id)
     form = provider.get_form('post')()
-    print form.validate_on_submit()
+    print request.method
+    print form.message.data
     if form.validate_on_submit():
         used_api = provider.use_api(form)
         flash(used_api)
