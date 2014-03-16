523b69f97f28397b3138f91cee3b855d759d6662
Jo Plaete <jo.plaete@gmail.com>
2014-03-16 16:15:27 +0000

    Update __init__.py
    typo in from flask.ext.social.utils import get_connection_values_from_oauth_response

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,9 @@
-
 from flask import redirect, url_for, session
 from flask.ext.assets import Environment
 from flask.ext.security import Security, SQLAlchemyUserDatastore
 from flask.ext.social import Social, SQLAlchemyConnectionDatastore, \
      login_failed
-from flask.ext.social.utils import get_conection_values_from_oauth_response
+from flask.ext.social.utils import get_connection_values_from_oauth_response
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from .helpers import Flask
